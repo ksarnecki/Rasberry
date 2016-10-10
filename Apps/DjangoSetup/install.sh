887d60669c247afe4267331aa5ee3201faa9a35f
sarna1994karol <sarna1994karol@gmail.com>
2016-10-10 15:28:10 +0800

    Install django packages

---
+++ Apps/DjangoSetup/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install python-django
+
+sudo apt-get install python-virtualenv
+
+sudo apt-get install python-pip
+
+sudo apt-get install ruby-full
+
+gem install sass
+
+sudo apt-get install postgresql
+
+sudo apt-get install postgresql-server-dev-9.4
+
+sudo apt-get install libxml2 libxslt-dev python-dev libxmlsec1-dev libxmlsec1
+
+sudo apt-get install libjpeg-dev
+
+sudo apt-get install python-cffi
+
+sudo apt-get install wkhtmltopdf
+
+
