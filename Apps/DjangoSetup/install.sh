f6360e18ed9908d9d11a646afd6648bf5cee6fac
sarna1994karol <sarna1994karol@gmail.com>
2016-10-10 16:48:10 +0800

    Save

--- Apps/DjangoSetup/install.sh
+++ Apps/DjangoSetup/install.sh
@@ -24,4 +24,31 @@ sudo apt-get install python-cffi
 
 sudo apt-get install wkhtmltopdf
 
+sudo pip install djangorestframework
+
+sudo pip install django-countries
+
+sudo apt-get install nodejs
+
+sudo apt-get install npm
+
+npm install --save-dev webpack-bundle-tracker
+
+sudo apt install postgresql-server-dev-all
+
+sudo apt install build-essential
+
+sudo apt-get install python-psycopg2
+
+sudo pip install python-dateutil --upgrade
+
+sudo pip install django-resized 
+
+sudo pip install ajax_select
+
+sudo apt-get install python-lxml
+
+sudo apt-get install python-oauthlib
+
+sudo apt-get install python-ajax-select
 
